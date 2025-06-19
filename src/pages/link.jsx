f8be2173227479caeaa98251fa31719498da9e17
Khushi84670 <khushijaiswal2289@gmail.com>
2025-06-19 15:00:36 +0530

    Update link.jsx

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -76,11 +76,11 @@ const LinkPage = () => {
             {url?.title}
           </span>
           <a
-            href={`https://url-shortner-main-gamma.vercel.app/${link}`}
+            href={`https://url-shortner-app-eta.vercel.app/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            https://url-shortner-main-gamma.vercel.app/{link}
+            https://url-shortner-app-eta.vercel.app/{link}
           </a>
           <a
             href={url?.original_url}
@@ -98,7 +98,7 @@ const LinkPage = () => {
               variant="ghost"
               onClick={() =>
                 navigator.clipboard.writeText(
-                  `https://url-shortner-main-gamma.vercel.app/${link}`,
+                  `https://url-shortner-app-eta.vercel.app/${link}`,
                 )
               }
             >
