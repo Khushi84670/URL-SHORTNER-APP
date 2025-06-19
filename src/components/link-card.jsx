351ab5c8affeebdecdef259883551945dcd053ef
Khushi84670 <khushijaiswal2289@gmail.com>
2025-06-19 14:58:41 +0530

    Update link-card.jsx

--- src/components/link-card.jsx
+++ src/components/link-card.jsx
@@ -40,7 +40,7 @@ const LinkCard = ({ url = [], fetchUrls }) => {
           {url?.title}
         </span>
         <span className="text-2xl text-blue-400 font-bold hover:underline cursor-pointer">
-          https://url-shortner-main-gamma.vercel.app/
+          https://url-shortner-app-eta.vercel.app/
           {url?.custom_url ? url?.custom_url : url.short_url}
         </span>
         <span className="flex items-center gap-1 hover:underline cursor-pointer">
@@ -56,7 +56,7 @@ const LinkCard = ({ url = [], fetchUrls }) => {
           variant="ghost"
           onClick={() =>
             navigator.clipboard.writeText(
-              `https://url-shortner-main-gamma.vercel.app/${url?.short_url}`,
+              `https://url-shortner-app-eta.vercel.app/${url?.short_url}`,
             )
           }
         >
